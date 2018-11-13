3199d7a0a4b3e9f95c0a1f1a701ac39f5e5f2ae2
Goncalo Franco <gapfranco@gmail.com>
2018-11-13 14:03:24 -0200

    Improvinf README

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const helmet = require("helmet");
 const block = require("./routes/block");
 
+const version = 1;
+const port = 8000;
+
 const app = express();
 
 app.use(express.json());
@@ -10,12 +13,11 @@ app.use(helmet());
 app.get("/", (req, res) => {
     res.json({
         "API": "blockchainWS",
-        "version": "1.0"
+        "version": version
     });
   });
 
 app.use("/block", block);
 
-const port = 8000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
